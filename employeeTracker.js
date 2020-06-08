6492d85690e7199d7f1e31d953158eabe28a95d1
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-08 11:25:55 -0400

    Added starter files, started js file

---
+++ employeeTracker.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: ""
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    startApp();
+});
