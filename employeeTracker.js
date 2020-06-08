12822bf3849294f472beabc36720af7a5d2a7476
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-08 11:51:47 -0400

    Added sql file and path

--- employeeTracker.js
+++ employeeTracker.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "rootroot",
-    database: ""
+    database: "employee_DB"
 });
 
 connection.connect(function(err) {
