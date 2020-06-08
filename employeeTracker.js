88b662649debeceb8b82055386ca2c170bfd2875
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-08 19:54:05 -0400

    added psudeo code

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,6 +1,9 @@
+//Required
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require('console.table');
 
+//Create mySQL connection
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,3 +16,25 @@ connection.connect(function(err) {
     if (err) throw err;
     startApp();
 });
+
+//Starts app
+
+    //Prompt user with questions
+
+    //Call fuction based on answer
+
+//Get array of roles
+
+//Add employee to database
+
+//Remove employee from database
+
+//View database
+
+//Update employee information
+
+//Add role entry
+
+//View roles
+
+//Remove roles
