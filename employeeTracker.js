cd4b7a923fa70a841bd625d6f7ff8bd62dd00dd9
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-08 20:43:27 -0400

    add prompt and functions

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,7 +1,7 @@
 //Required
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const cTable = require('console.table');
+//const cTable = require('console.table');
 
 //Create mySQL connection
 const connection = mysql.createConnection({
@@ -12,42 +12,83 @@ const connection = mysql.createConnection({
     database: "employee_DB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     startApp();
 });
 
 //Starts app
 function startApp() {
-    inquirer.prompt({
-        name: "",
-        type: "list",
-        message: "What would you like to do?",
-        choices: ["Add Employee", 
-        "Remove Employee", 
-        "View All Employees", 
-        "Update Employee Role",
-        "Add Role",
-        "Remove Role",
-        "EXIT"]
-    })
-}
     //Prompt user with questions
-
+    inquirer.prompt({
+        name: "options",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: ["Add Employee",
+            "Remove Employee",
+            "View All Employees",
+            "Add Role",
+            "Remove Role",
+            "Update Employee Role",
+            "Add Department",
+            "Remove Department",
+            "EXIT"]
+    })
     //Call fuction based on answer
+        .then(function (option) {
+            if (option.choice === "Add Employee") {
+                addEmployee();
+            } else if (option.choice === "Remove Employee") {
+                removeEmployee();
+            } else if (option.choice === "View All Employees") {
+                viewEmployees();
+            } else if (option.choice === "Add Role") {
+                addRole();
+            } else if (option.choice === "Remove Role") {
+                removeRole();
+            } else if (option.choice === "Update Employee Role") {
+                updateRole();
+            } else if (option.choice === "Add Department") {
+                addDep();
+            } else if (option.choice === "Remove Department") {
+                removeDep();
+            } else if (option.choice === "EXIT") {
+                connection.end();
+            }
+        })
+}
 
 //Get array of roles
 
 //Add employee to database
+addEmployee() {
 
+};
 //Remove employee from database
-
+removeEmployee() {
+    
+};
 //View database
-
-//Update employee information
-
-//Add role entry
-
-//View roles
-
+viewEmployees() {
+    
+};
+//Add role
+addRole() {
+    
+};
+//Remove role
+removeRole() {
+    
+};
 //Update employee roles
+updateRole() {
+    
+};
+//Add department
+addDep() {
+
+};
+//Remove department
+removeDep() {
+
+};
