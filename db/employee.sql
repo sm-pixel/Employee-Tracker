f1ada9c8b856f0ef1dd9f3f024dc9ae18fe776d9
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-22 12:10:07 -0400

    updated employee.sql to match local schema on SQL

--- db/employee.sql
+++ db/employee.sql
@@ -14,14 +14,14 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id),
-    );
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    manager_id INT,
-    role_id INT,
-    PRIMARY KEY (id),
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
 );
